5ddc03a1ac8dafe24c24d2ee6ccf550e91bf2f64
Silver-3 <174689796+Silver-3@users.noreply.github.com>
2025-01-25 14:38:17 +1100

    Update 2.1.0 (Added JSDoc comments)

--- src/maps/index.js
+++ src/maps/index.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 
 const maps = {ranked: {}, nonranked: {}};
 
+/**
+ * @typedef {Object} mapObject
+ * @property {string} name 
+ * @property {string} image 
+ */
+
 fs.readdirSync(__dirname).forEach(type => {
     const typeDir = path.join(__dirname, type);
 
@@ -18,6 +24,12 @@ fs.readdirSync(__dirname).forEach(type => {
     }
 });
 
+/**
+ * 
+ * @param {String} type 
+ * @returns {mapObject}
+ */
+
 function randomMap(type) {
     if (!type) throw new Error("Team type is required");
     type = type.toLowerCase().replace(' ', '');
@@ -26,6 +38,12 @@ function randomMap(type) {
     const keys = Object.keys(maps[type]);
     return maps[type][keys[Math.floor(Math.random() * keys.length)]];
 }
+
+/**
+ * 
+ * @param {String} mapName 
+ * @returns {mapObject}
+ */
 
 function getMap(mapName) {
     if (!mapName) throw new Error("Map name is required");
@@ -40,17 +58,32 @@ function getMap(mapName) {
     throw new Error(`Map (${mapName}) not found.`);
 }
 
+/**
+ * 
+ * @returns {Array<mapObject>}
+ */
+
 function getRankedMaps() {
     return Object.keys(maps.ranked).map(map => ({
         [map]: maps.ranked[map]
     }));
 }
 
+/**
+ * 
+ * @returns {Array<mapObject>}
+ */
+
 function getNonrankedMaps() {
     return Object.keys(maps.nonranked).map(map => ({
         [map]: maps.nonranked[map]
     }));
 }
+
+/**
+ * 
+ * @returns {Array<mapObject>}
+ */
 
 function getAllMaps() {
     let mapArray = [];
--- src/weapons/index.js
+++ src/weapons/index.js
@@ -3,6 +3,21 @@ const fs = require('fs');
 
 const weapons = {};
 
+/**
+ * @typedef {Object} weaponObject
+ * @property {string} name
+ * @property {string} image 
+ * @property {Object} stats 
+ * @property {number} stats.damage
+ * @property {number} stats.firerate 
+ * @property {number} stats.ammo
+ * @property {number} stats.maxammo
+ * @property {number} stats.difficulty
+ * @property {string} type
+ * @property {string[]} scopes
+ * @property {string[]} operators
+ */
+
 fs.readdirSync(__dirname).forEach(weapon => {
     const weaponDir = path.join(__dirname, weapon);
 
@@ -12,10 +27,21 @@ fs.readdirSync(__dirname).forEach(weapon => {
     }
 });
 
+/**
+ * 
+ * @returns {weaponObject}
+ */
+
 function randomWeapon() {
     const keys = Object.keys(weapons);
     return weapons[keys[Math.floor(Math.random() * keys.length)]];
 }
+
+/**
+ * 
+ * @param {String} weapon 
+ * @returns {weaponObject}
+ */
 
 function getWeapon(weapon) {
     if (!weapon) throw new Error("Weapon name is required");
@@ -26,6 +52,11 @@ function getWeapon(weapon) {
 
     return weapons[weaponsA];
 }
+
+/**
+ * 
+ * @returns {Array<weaponObject>}
+ */
 
 function getAllWeapons() {
     let weaponArray = [];
